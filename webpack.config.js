9e8d8154d441db94c9d2955873a585fa293ceee8
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-06-18 01:17:53 -0500

    change output filename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-      filename: 'main.js',
+      filename: 'input-tag.min.js',
       path: path.resolve(__dirname, 'dist'),
     },
     module: {
